ce012c90d932a4b5a470d2aa483d1f1e6010a91d
Doug Davis <s12.2@davis-laptop.gateway.2wire.net>
2012-12-07 09:19:06 -0500

    initial project import from KDevelop

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QtGui/QApplication>
+#include "pde1d.h"
+
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    pde1d foo;
+    foo.show();
+    return app.exec();
+}
---
+++ pde1d.h
@@ -0,0 +1,14 @@
+#ifndef pde1d_H
+#define pde1d_H
+
+#include <QtGui/QMainWindow>
+
+class pde1d : public QMainWindow
+{
+Q_OBJECT
+public:
+    pde1d();
+    virtual ~pde1d();
+};
+
+#endif // pde1d_H
